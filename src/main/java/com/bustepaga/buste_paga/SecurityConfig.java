05bd94feb9f6dd80fb14dfb6ede0b02cf78457a9
Diego <diegodesign9@gmail.com>
2025-07-15 19:35:34 +0200

    Auto-sync: file changes

--- src/main/java/com/bustepaga/buste_paga/SecurityConfig.java
+++ src/main/java/com/bustepaga/buste_paga/SecurityConfig.java
@@ -22,6 +22,6 @@ public class SecurityConfig {
 
     @Bean
     public UserDetailsService userDetailsService() {
-        return new InMemoryUserDetailsManager(); // UserDetailsService vuoto per evitare errori
+        return new InMemoryUserDetailsManager(); // UserDetailsService vuoto per evitare error
     }
 }
