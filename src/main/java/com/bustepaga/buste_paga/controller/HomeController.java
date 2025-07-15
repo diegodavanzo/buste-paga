e99d18cc1d5d50c5bbde4630c5bdcf2e0dc5d03c
Diego <diegodesign9@gmail.com>
2025-07-15 21:52:58 +0200

    Auto-sync: file changes

--- src/main/java/com/bustepaga/buste_paga/controller/HomeController.java
+++ src/main/java/com/bustepaga/buste_paga/controller/HomeController.java
@@ -5,8 +5,9 @@ import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class HomeController {
+
     @GetMapping("/home")
     public String home() {
-        return "home"; // Thymeleaf: src/main/resources/templates/home.html
+        return "home"; // corrisponde a home.html
     }
 }
